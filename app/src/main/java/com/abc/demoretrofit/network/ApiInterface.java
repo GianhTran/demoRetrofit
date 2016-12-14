4a575e4d8ef14f6f2ee496aca4604766d5776a31
GianhTran <tran.nguyen.song.gianh@framgia.com>
2016-12-14 20:52:51 +0700

    update project

--- app/src/main/java/com/abc/demoretrofit/network/ApiInterface.java
+++ app/src/main/java/com/abc/demoretrofit/network/ApiInterface.java
@@ -1,23 +1,17 @@
 package com.abc.demoretrofit.network;
 
-import com.abc.demoretrofit.model.Data;
 import com.abc.demoretrofit.model.LoginWrapper;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 /**
  * Created by framgia on 14/12/2016.
  */
 
 public interface ApiInterface {
-    @GET("weather")
-    Call<Data> loadWeather(@Query("apikey") String apiKey, @Query("lat") double lat,
-            @Query("lon") double lon);
 
     @FormUrlEncoded
     @Headers({
